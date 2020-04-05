b30d0a5e1aaacee4578608a3cbd977346c9d899d
solidest <solidest@163.com>
2020-04-05 21:38:25 +0800

    update

--- test/proj_protocol_test/program/demo1.lua
+++ test/proj_protocol_test/program/demo1.lua
@@ -1,49 +1,46 @@
 
-function Test_assert_verify(a)
-    --verify assert
-    verify(a>0, '???')
-    assert(a>0)
-    verify(a==0, '验证失败，继续执行')
-    assert(a==0, '断言失败，退出执行')
-end
-
+--print
 function Test_print(v1, v2)
-    --print
     print("Hello World1")
     print('v1:', v1, 'v2:', v2)
+    print('::test print::', 'ok')
 end
 
+--verify assert
+function Test_assert_verify(a)
+    verify(a>0, '???')
+    assert(a>0)
+    verify(a==0, '测试验证失败，继续执行')
+    print('::test assert verify::', 'ok')
+    assert(a==0, '测试断言失败，退出执行')
+end
+
+--string.hex string.bin
 function Test_hex_buf()
-    --string.hex string.bin
-    local str = 'FAE298E6ACDF3D';
-    local strbuf1 = string.buf(str)
-    print('strbuf.len:', string.len(strbuf1))
-    local strbuf2 = string.hex(strbuf1)
-    print('strhex: ', strbuf2)
-    assert(str == strbuf2)
+    local str1 = 'FAE298E6ACDF3D';
+    local strbuf = string.buf(str1)
+    assert(string.len(str1)/2==string.len(strbuf))
+
+    local str2 = string.hex(strbuf)
+    assert(str1 == str2)
+    print('::test string.hex string.buf::', 'ok')
 end
 
+--delay
 function Test_delay()
     print('delay 2s')
     delay(2000)
     print('delay 3s')
     delay(3000)
+    print('::test delay::', 'ok')
 end
 
+--入口函数
 function entry(vars, option)
-    print('\n::test print::')
     Test_print(vars, option)
-
-    print('\n::test string.hex string.buf::')
     Test_hex_buf()
-
-    print('\n::test delay::')
     Test_delay()
-
-    print('\n::test assert verify::')
-    local i1 = 1;
-    Test_assert_verify(i1)
-    
+    Test_assert_verify(1)
     exit()
 end
 
